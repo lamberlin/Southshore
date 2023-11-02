20ebbd4ddf69640310bcd1b99234cff350ed333d
Bei Lin <129919638+lamberlin@users.noreply.github.com>
2023-11-01 23:17:22 -0700

    Update map.py

--- map.py
+++ map.py
@@ -6,7 +6,7 @@ import geopandas as gpd
 
 
 # Setup
-background_image = "https://drive.google.com/file/d/1ULOlggWoTgSd6EQfhAMoK1m9NdrD6PLJ/view?usp=sharing"
+background_image = "https://drive.google.com/uc?export=view&id=1ULOlggWoTgSd6EQfhAMoK1m9NdrD6PLJ"
 
 st.markdown(
     f"""
